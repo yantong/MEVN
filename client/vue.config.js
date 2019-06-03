26f99688fdcddaee4884a911e6bfbb286b797268
yantong <1209144177@qq.com>
2019-06-03 16:28:33 +0800

    配置config dist-clean-plugin，消除eslint

---
+++ client/vue.config.js
@@ -0,0 +1,17 @@
+const { CleanWebpackPlugin }= require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    // 基本路径
+    baseUrl: '/',
+    // 输出文件目录
+    outputDir: '../server/dist',
+    lintOnSave: false,
+    configureWebpack: {
+        plugins: [
+            new CleanWebpackPlugin({
+                root: path.join(__dirname, '../server/dist')
+            })
+        ]
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,21 @@
+//express_demo.js 文件
+var express = require('express');
+var app = express();
+const path = require('path');
+
+ 
+app.use(express.static(path.join(__dirname,'dist')));
+app.get('/',(req,res) =>{
+    res.sendFile('./dist/index.html');
+});
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname,'dist/index.html'));
+})
+  
+
+var server = app.listen(3000, function () {
+    var port = server.address().port
+   
+    console.log("应用实例，访问地址为 http://localhost:%s", port)
+})
